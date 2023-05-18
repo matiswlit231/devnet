6145f1c665e01b9d7939fcac60e54419bff61a4c
matiswlit231 <matias.godoy231s@gmail.com>
2023-05-18 04:44:20 +0000

    agregado script bash

---
+++ script.sh
@@ -0,0 +1,11 @@
+echo "FROM python" > Dockerfile
+echo "RUN apt-get update" >> Dockerfile
+echo "RUN apt-get install -y install python3-pip" >> Dockerfile
+echo "RUN pip install flask" >> Dockerfile
+echo "COPY index.html" /home/app/templates/" >> Dockerfile
+echo "COPY conect.py" /home/app/" >> Dockerfile
+echo "EXPOSE 8000" : puerto >> Dockerfile
+echo "CMD python3 /home/app/conect.py" >> Dockerfile
+docker build -t app2 .
+docker run -t -d -p 8080:8080 --name app2 app2
+docker ps

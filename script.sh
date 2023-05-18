db12442bdb0a77ed8de853f4df078b344d756eb7
matiswlit231 <matias.godoy231s@gmail.com>
2023-05-18 05:12:54 +0000

    quinto comentario

--- script.sh
+++ script.sh
@@ -6,6 +6,6 @@ echo "COPY index.html" /home/app/templates/" >> Dockerfile
 echo "COPY conect.py" /home/app/" >> Dockerfile
 echo "EXPOSE 8000" : puerto >> Dockerfile
 echo "CMD python3 /home/app/conect.py" >> Dockerfile
-docker build -t app2 .
-docker run -t -d -p 8080:8080 --name app2 app2
+docker build -t app .
+docker run -t -d -p 8080:8080 --name app app
 docker ps
